da99d58ce757aae4e4637668be3704b31fc4f096
Jin <jcguu95@gmail.com>
2019-05-25 11:04:22 -0400

    succeeded changing from looking at the file_name to looking at the 'exiftool identifier'

--- arXiv-helper.sh
+++ arXiv-helper.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
-## InputFile must be in the example form:
-## "1808.01059.pdf"		# I call it type A.
-## "math---0503727.pdf"		# I call it type B.
-## "q-alg---9503006.pdf"	# I call it type B.
+## Dependencies: internet connection, exiftool, wget
 
-## Usage: Simply put all files from arXiv in a folder, make sure
-## 	  the name of the files are correct (as above), and run
-## 	  for example:
-## 	  	$ arXiv-helper 1808.01059.pdf
-##  	  or for doing it in patch:
-## 	  	$ for i in *.pdf; do arXiv-helper "$i"; done
+## Input file must have identifier in the form:
+##	 arXiv:[archive].[subject-class]/YYMM[number]{vV}
+## 	 arXiv:YYMM.number{vV}
+## For example 	arXiv:math.GT/0309136	or
+##		arXiv:1501.00001	or
+##		arXiv:0706.0001v2	.
+## For more information about arXiv identifier, please refer to the official site:
+## 	Understanding the arXiv identifier (https://arxiv.org/help/arxiv_identifier)
+## To check the identifier of the file, enter the command
+## 	$ exiftool TheFileName.pdf -Identifier
 
 ## TODO
-## 1.   I feel it is better to identify the file itself
-##    by looking at its arXiv-id in the metadata, rather
-##    than at its file name. Perhaps I will make a change.
-## 2.   Before '1.' gets done, notice that the current script
-##    has a drawback that it does not distinguish Type "A" files
-##    and Type "neither" files quite well..
-## 3.   Though it is quite important, the current script
+## 1.   Though it is quite important, the current script
 ##    does not pull the version info down! This will be updated
 ##    in the future.
-## 4.   Write an arXiv downloader: only thing to do is to
+## 2.   Write an arXiv downloader: only thing to do is to
 ##    input arXiv id!
-## 5.   Add description: dependencies-- exiftool and wget.
 
 InputFile=$1
 Identifier=$(exiftool $1 -identifier | sed 's/Identifier *: //')
@@ -39,55 +33,17 @@ case $Identifier in # TODO: the regex is not good. fix it!
 		exit 1 ;;
 esac
 
-ArXivIdentifierWithoutBackSlash=$(echo "$Identifier" | sed 's/\//---/' ) # Change '\' to '---' in those of Type-Prior2007.
+ArXivIdentifierAvoidingBackSlash=$(echo "$Identifier" | sed 's/\//--/' ) # Change '\' to '--' in those of Type-Prior2007.
 ArXivIdentifierWithoutVersion=$(echo "$Identifier" | sed 's/arXiv://' | sed 's/v[0-9]*//' ) # TODO: check if this regex is correct
 ArXivMetaDataSiteURL="http://export.arxiv.org/oai2?verb=GetRecord&metadataPrefix=arXivRaw&identifier=oai:arXiv.org:$ArXivIdentifierWithoutVersion"
-TempMetaDataFileName="tmp.$ArXivIdentifierWithoutBackSlash.xml"
+TempMetaDataFileName="tmp.$ArXivIdentifierAvoidingBackSlash.xml"
 wget -O "$TempMetaDataFileName" "$ArXivMetaDataSiteURL" # Fetch metadata from the URL and output as an .xml file.
 MetaContent=$(echo $(<"$TempMetaDataFileName") | sed 's/\n//g') # The newlines are cut-off.
-
-#	ArXivID=$(echo $InputFile | sed 's/\.pdf//')
-#
-#	#
-#	# Determine which type is the arXiv-id
-#	case $ArXivID in
-	#	*"."*) Type="A" ;;
-	#	*"---"*) Type="B" ;;
-	#	*) Type="neither" ;;
-#	esac
-#
-#
-#	# Download the metadata online (export.arxiv.org/...) with respect to Type (A, B, or neither)
-#	case $Type in # require internet connection
-	#	"A")
-		#	echo "It is of type A."
-		#	ActualAxXivID=$ArXivID
-		#	wget -O "tmp.$ArXivID.xml" "http://export.arxiv.org/oai2?verb=GetRecord&metadataPrefix=arXivRaw&identifier=oai:arXiv.org:$ActualAxXivID"
-		#	;;
-#
-	#	"B")
-		#	echo "It is of type B"
-		#	ArXivIDTypeB1=$(echo $ArXivID | sed 's/---.*//' )
-		#	ArXivIDTypeB2=$(echo $ArXivID | sed 's/.*---//' )
-		#	ActualAxXivID="$ArXivIDTypeB1/$ArXivIDTypeB2"
-		#	echo "Type b: B1=$ArXivIDTypeB1 ; B2=$ArXivIDTypeB2 "
-		#	wget -O "tmp.$ArXivID.xml" "http://export.arxiv.org/oai2?verb=GetRecord&metadataPrefix=arXivRaw&identifier=oai:arXiv.org:$ActualAxXivID"
-		#	;;
-#
-	#	"neither")
-		#	echo "ERROR: File name is neither of type A nor B.. exit 1!" ;
-		#	exit 1 ;;
-#	esac
-#
-#
-#	# Input data from the downloaded metafiles.
-#	MetaContent=$(echo $(<tmp.$ArXivID.xml) | sed 's/\n//g')
 
 Title=$(echo $MetaContent | sed 's/.*<title>//' | sed 's/<\/title>.*//')
 Authors=$(echo $MetaContent | sed 's/.*<authors>//' | sed 's/<\/authors>.*//')
 Categories="arXiv: $(echo $MetaContent | sed 's/.*<categories>//' | sed 's/<\/categories>.*//')"
 Abstract=$(echo $MetaContent | sed 's/.*<abstract>//' | sed 's/<\/abstract>.*//')
-
 ### Check if d.o.i exists
 case $MetaContent in
 	*"<doi>"*"</doi>"*)
@@ -97,9 +53,7 @@ case $MetaContent in
 		DoiExist="false"
 		Doi="unknown" ;;
 esac
-
-echo "ArXivID = $ArXivID"
-echo "ActualAxXivID = $ActualAxXivID" # Differ from ARXIV_ID if it is of type B.
+echo "ArXivIdentifier = $Identifier"
 echo "Title= $Title"
 echo "Authors = $Authors"
 echo "Categories = $Categories"
@@ -108,16 +62,15 @@ echo "DoiExist = $DoiExist"
 echo "Doi = $Doi"
 
 # Change metadata of our .pdf files with 'exiftool'.
-exiftool $InputFile -title="$Title" -author="$Authors" -categories="$Categories" -doi="$Doi" -description="arXiv-id: $ActualAxXivID. Abstract: $Abstract"
+exiftool $InputFile -title="$Title" -author="$Authors" -categories="$Categories" -doi="$Doi" -description="[$Identifier] [Abstract] $Abstract"
 
 
 # Rename the file
-NewFileName=$(echo "[$Authors]-$Title---[arXiv-id-$ArXivID].pdf")
+NewFileName=$(echo "[$Authors]-$Title-[$ArXivIdentifierAvoidingBackSlash].pdf")
 echo "new file name = $NewFileName"
-## Renaming the files.
-cp "$InputFile" "$NewFileName"
+cp "$InputFile" "$NewFileName" # Renaming the files.
 mv "$InputFile""_original" "$InputFile"
 # Removing the temporary files.
-rm -rf tmp.$ArXivID.xml
+rm -rf $TempMetaDataFileName
 
 exit
