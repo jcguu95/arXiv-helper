93aebdb2f806cc2e774b1f8cf0478f4af02fb37e
Jin <jcguu95@gmail.com>
2019-05-25 09:37:26 -0400

    1. No longer replacing spaces by '-' in NewFileNames. 2. Change variable names from all capital.

--- arXiv-helper.sh
+++ arXiv-helper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## INPUTFILE must be in the example form:
+## InputFile must be in the example form:
 ## "1808.01059.pdf"		# I call it type A.
 ## "math---0503727.pdf"		# I call it type B.
 ## "q-alg---9503006.pdf"	# I call it type B.
@@ -17,43 +17,42 @@
 ##    by looking at its arXiv-id in the metadata, rather
 ##    than at its file name. Perhaps I will make a change.
 ## 2.   Before '1.' gets done, notice that the current script
-##    has a drawback that it does not distinguish TYPE "A" files
-##    and TYPE "neither" files quite well..
+##    has a drawback that it does not distinguish Type "A" files
+##    and Type "neither" files quite well..
 ## 3.   Though it is quite important, the current script
 ##    does not pull the version info down! This will be updated
 ##    in the future.
-## 4.   Am I sure that I want to change spaces to '-'?
-## 5.   Write an arXiv downloader: only thing to do is to
+## 4.   Write an arXiv downloader: only thing to do is to
 ##    input arXiv id!
 
 
-INPUT_FILE=$1
-ARXIV_ID=$(echo $INPUT_FILE | sed 's/\.pdf//')
+InputFile=$1
+ArXivID=$(echo $InputFile | sed 's/\.pdf//')
 
 
 # Determine which type is the arXiv-id
-case $ARXIV_ID in
-	*"."*) TYPE="A" ;;
-	*"---"*) TYPE="B" ;;
-	*) TYPE="neither" ;;
+case $ArXivID in
+	*"."*) Type="A" ;;
+	*"---"*) Type="B" ;;
+	*) Type="neither" ;;
 esac
 
 
-# Download the metadata online (export.arxiv.org/...) with respect to TYPE (A, B, or neither)
-case $TYPE in # require internet connection
+# Download the metadata online (export.arxiv.org/...) with respect to Type (A, B, or neither)
+case $Type in # require internet connection
 	"A")
 		echo "It is of type A."
-		ACTUAL_ARXIV_ID=$ARXIV_ID
-		wget -O "tmp.$ARXIV_ID.xml" "http://export.arxiv.org/oai2?verb=GetRecord&metadataPrefix=arXivRaw&identifier=oai:arXiv.org:$ACTUAL_ARXIV_ID"
+		ActualAxXivID=$ArXivID
+		wget -O "tmp.$ArXivID.xml" "http://export.arxiv.org/oai2?verb=GetRecord&metadataPrefix=arXivRaw&identifier=oai:arXiv.org:$ActualAxXivID"
 		;;
 
 	"B")
 		echo "It is of type B"
-		ARXIV_ID_TYPE_B1=$(echo $ARXIV_ID | sed 's/---.*//' )
-		ARXIV_ID_TYPE_B2=$(echo $ARXIV_ID | sed 's/.*---//' )
-		ACTUAL_ARXIV_ID="$ARXIV_ID_TYPE_B1/$ARXIV_ID_TYPE_B2"
-		echo "Type b: B1=$ARXIV_ID_TYPE_B1 ; B2=$ARXIV_ID_TYPE_B2 "
-		wget -O "tmp.$ARXIV_ID.xml" "http://export.arxiv.org/oai2?verb=GetRecord&metadataPrefix=arXivRaw&identifier=oai:arXiv.org:$ACTUAL_ARXIV_ID"
+		ArXivIDTypeB1=$(echo $ArXivID | sed 's/---.*//' )
+		ArXivIDTypeB2=$(echo $ArXivID | sed 's/.*---//' )
+		ActualAxXivID="$ArXivIDTypeB1/$ArXivIDTypeB2"
+		echo "Type b: B1=$ArXivIDTypeB1 ; B2=$ArXivIDTypeB2 "
+		wget -O "tmp.$ArXivID.xml" "http://export.arxiv.org/oai2?verb=GetRecord&metadataPrefix=arXivRaw&identifier=oai:arXiv.org:$ActualAxXivID"
 		;;
 
 	"neither")
@@ -63,54 +62,43 @@ esac
 
 
 # Input data from the downloaded metafiles.
-META_CONTENT=$(echo $(<tmp.$ARXIV_ID.xml) | sed 's/\n//g')
+MetaContent=$(echo $(<tmp.$ArXivID.xml) | sed 's/\n//g')
 
-TITLE=$(echo $META_CONTENT | sed 's/.*<title>//' | sed 's/<\/title>.*//')
-AUTHORS=$(echo $META_CONTENT | sed 's/.*<authors>//' | sed 's/<\/authors>.*//')
-CATEGORIES="arXiv: $(echo $META_CONTENT | sed 's/.*<categories>//' | sed 's/<\/categories>.*//')"
-ABSTRACT=$(echo $META_CONTENT | sed 's/.*<abstract>//' | sed 's/<\/abstract>.*//')
+Title=$(echo $MetaContent | sed 's/.*<title>//' | sed 's/<\/title>.*//')
+Authors=$(echo $MetaContent | sed 's/.*<authors>//' | sed 's/<\/authors>.*//')
+Categories="arXiv: $(echo $MetaContent | sed 's/.*<categories>//' | sed 's/<\/categories>.*//')"
+Abstract=$(echo $MetaContent | sed 's/.*<abstract>//' | sed 's/<\/abstract>.*//')
 
 ### Check if d.o.i exists
-case $META_CONTENT in
+case $MetaContent in
 	*"<doi>"*"</doi>"*)
-		DOI_EXIST="true"
-		DOI=$(echo $META_CONTENT | sed 's/.*<doi>//' | sed 's/<\/doi>.*//') ;;
+		DoiExist="true"
+		Doi=$(echo $MetaContent | sed 's/.*<doi>//' | sed 's/<\/doi>.*//') ;;
 	*)
-		DOI_EXIST="false"
-		DOI="unknown" ;;
+		DoiExist="false"
+		Doi="unknown" ;;
 esac
 
-echo "ARXIV_ID = $ARXIV_ID"
-echo "ACTUAL_ARXIV_ID = $ACTUAL_ARXIV_ID" # Differ from ARXIV_ID if it is of type B.
-echo "TITLE = $TITLE"
-echo "AUTHORS = $AUTHORS"
-echo "CATEGORIES = $CATEGORIES"
-echo "ABSTRACT = $ABSTRACT"
-echo "DOI_EXIST = $DOI_EXIST"
-echo "DOI = $DOI"
+echo "ArXivID = $ArXivID"
+echo "ActualAxXivID = $ActualAxXivID" # Differ from ARXIV_ID if it is of type B.
+echo "Title= $Title"
+echo "Authors = $Authors"
+echo "Categories = $Categories"
+echo "Abstract = $Abstract"
+echo "DoiExist = $DoiExist"
+echo "Doi = $Doi"
 
 # Change metadata of our .pdf files with 'exiftool'.
-exiftool $INPUT_FILE -title="$TITLE" -author="$AUTHORS" -categories="$CATEGORIES" -doi="$DOI" -description="arXiv-id: $ACTUAL_ARXIV_ID. Abstract: $ABSTRACT"
+exiftool $InputFile -title="$Title" -author="$Authors" -categories="$Categories" -doi="$Doi" -description="arXiv-id: $ActualAxXivID. Abstract: $Abstract"
 
 
 # Rename the file
-### For the authors part
-### special characters (currently '(' and ')') and spaces are transformed into '-'
-### ', ' and ',' are transformed into '_'.
-### AUTHORS=$(echo "$AUTHORS" | sed 's/, /_/g' | sed 's/,/_/g' | sed 's/ /-/g' | sed 's/(/-/g' | sed 's/)/-/g')
-### For the title part
-### special characters (currently '(' and ')') and spaces are transformed into '-'
-### ', ' and ',' are transformed into ''.
-### TITLE=$(echo "$TITLE" | sed 's/ /-/g' | sed 's/(/-/g' | sed 's/)/-/g' | sed 's/,//g')
-AUTHORS=$(echo "$AUTHORS" | sed 's/ /-/g')
-TITLE=$(echo "$TITLE" | sed 's/ /-/g' )
-## Finally, we got the new name.
-NEW_FILE_NAME=$(echo "[$AUTHORS]-$TITLE---[arXiv-id-$ARXIV_ID].pdf")
-echo "new file name = $NEW_FILE_NAME"
+NewFileName=$(echo "[$Authors]-$Title---[arXiv-id-$ArXivID].pdf")
+echo "new file name = $NewFileName"
 ## Renaming the files.
-cp "$INPUT_FILE" "$NEW_FILE_NAME"
-mv "$INPUT_FILE""_original" "$INPUT_FILE"
+cp "$InputFile" "$NewFileName"
+mv "$InputFile""_original" "$InputFile"
 # Removing the temporary files.
-rm -rf tmp.$ARXIV_ID.xml
+rm -rf tmp.$ArXivID.xml
 
 exit
